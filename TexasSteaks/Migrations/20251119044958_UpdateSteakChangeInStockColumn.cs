6a8a07f835456dde732c85f23746dc1deddb2ac7
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-19 03:22:08 -0300

    Migration to update data in the InStock column of the Steak table.

---
+++ TexasSteaks/Migrations/20251119044958_UpdateSteakChangeInStockColumn.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TexasSteaks.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSteakChangeInStockColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 4");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 0 WHERE Id = 5");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 6");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 7");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 0 WHERE Id = 8");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 9");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 10");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 11");
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 1 WHERE Id = 12");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Steaks SET InStock = 0");
+        }
+    }
+}
