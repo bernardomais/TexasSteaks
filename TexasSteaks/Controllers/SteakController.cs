f067dc13918c5eb1715de24fbc388fbcb6bf06e1
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-19 03:34:23 -0300

    Adding the SteakController and the List view to display the products.

---
+++ TexasSteaks/Controllers/SteakController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TexasSteaks.Repositories.Interfaces;
+
+namespace TexasSteaks.Controllers
+{
+    public class SteakController : Controller
+    {
+        private readonly ISteakRepository _steakRepository;
+
+        public SteakController(ISteakRepository steakRepository)
+        {
+            _steakRepository = steakRepository;
+        }
+
+        public IActionResult List()
+        {
+            var steaks = _steakRepository.Steaks;
+            return View(steaks);
+        }
+    }
+}
