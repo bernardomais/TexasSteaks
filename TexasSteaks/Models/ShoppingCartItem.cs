9f148a9fae773610f75ff5e76b9cac1d970c0a83
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-24 18:25:04 -0300

    Creating model ShoppingCartItem.

---
+++ TexasSteaks/Models/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TexasSteaks.Models
+{
+    public class ShoppingCartItem
+    {
+        public int Id { get; set; }
+        
+        public Steak Steak { get; set; }
+        
+        public int Amount { get; set; }
+        
+        [StringLength(200)]
+        public string ShoppingCartId { get; set; }
+    }
+}
