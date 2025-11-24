0f169b4bd9f2a05e4d18f99257b984cb9d9ebb12
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-24 18:28:36 -0300

    Registering the DbSet for ShoppingCartItem.

--- TexasSteaks/Context/AppDbContext.cs
+++ TexasSteaks/Context/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace TexasSteaks.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Steak> Steaks { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
