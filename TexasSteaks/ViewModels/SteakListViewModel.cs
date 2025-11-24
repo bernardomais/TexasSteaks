07a73bb8bb717e359d73101f63fc5c4d754c641a
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-24 14:16:38 -0300

    Creating a ViewModel for the list of Steaks.

---
+++ TexasSteaks/ViewModels/SteakListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TexasSteaks.Models;
+
+namespace TexasSteaks.ViewModels
+{
+    public class SteakListViewModel
+    {
+        public IEnumerable<Steak> Steaks { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
